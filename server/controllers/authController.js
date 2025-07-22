3033f1052df03c3f65b1e80e3af04eef8f462362
ritesh sharama <riteshsharma1897299@gmail.com>
2025-07-22 23:08:51 +0530

    login and logout

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -26,10 +26,81 @@ export const register = async (req,res) => {
 
     await user.save()
 
-    const token = jwt.sign({id: user._id},)
+    const token = jwt.sign({id: user._id}, process.env.JWT_SECRET,{expiresIn:'7d'})
+
+    res.cookie('token',token, {
+      httpOnly:true,
+      secure:process.env.NODE_ENV === 'production',
+      sameSite:process.env.NODE_ENV === 'production'? 'none':'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000
+    })
     
   } catch (error) {
     res.json({success:false,message:error.message})
+    
+  }
+  
+}
+
+export const login = async (req,res) => {
+  const {email,password} = req.body;
+
+  if(!email || !password){
+    return res.json({success:false,message:'Email and password are reqired'})
+  }
+  try {
+
+    const user  = await userMOdel.findOne({email})
+
+    if(!user){
+      return res.json({success:false,message:'Invalid email'})
+    }
+
+    const isMatch = bcrypt.compare(password,user.password);
+
+    if(!isMatch){
+      return res.json({success:false,message:'Invalid password'})
+
+
+    }
+    const token = jwt.sign({id: user._id}, process.env.JWT_SECRET,{expiresIn:'7d'})
+
+    res.cookie('token',token, {
+      httpOnly:true,
+      secure:process.env.NODE_ENV === 'production',
+      sameSite:process.env.NODE_ENV === 'production'? 'none':'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000
+    })
+    return res.json({success:true})
+    
+  } catch (error) {
+    
+   return res.json({success:false,message:error.message})
+
+
+    
+  }
+
+
+  
+}
+
+
+export const logout = async (req,res) => {
+  try {
+    res.clearCookie('token',{
+      httpOnly:true,
+      secure:process.env.NODE_ENV === 'production',
+      sameSite:process.env.NODE_ENV === 'production'? 'none':'strict'
+      
+
+    })
+
+    return res.json({success:true,message:"Logged Out"})
+    
+  } catch (error) {
+   return res.json({success:false,message:error.message})
+
     
   }
   
